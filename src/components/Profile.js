5b3fb0219ba9dcdfc69f30358b4aa31f0cc78e9d
antoninaorobets <pani.antonina.don@gmail.com>
2022-02-23 12:55:36 -0500

    commit tree 9388ed56ed1b281a67fae95dc9ddc5f26805ea1a

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
 import Interests from "./Interests";
+import { UserContext } from "../context/user";
+import {ThemeContext} from "../context/theme"
 
-function Profile({ user, theme }) {
+function Profile() {
+  const {user} = useContext(UserContext)
+   const {theme} = useContext(ThemeContext)
+
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
